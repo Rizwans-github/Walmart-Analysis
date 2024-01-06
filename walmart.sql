59a4194b7c93555ae32b337cd98bc1ed979570b0
Mohd Rizwan <141806496+Rizwans-github@users.noreply.github.com>
2024-01-06 16:55:58 +0530

    a

--- walmart.sql
+++ walmart.sql
@@ -1,11 +1,52 @@
+CREATE DATABASE walmart;
+USE walmart;
+
+CREATE TABLE walmart_data(
+   Invoice_ID         INT(10)  NOT NULL PRIMARY KEY 
+  ,Branch                    VARCHAR(1) NOT NULL
+  ,City                      VARCHAR(9) NOT NULL
+  ,Customer_type           VARCHAR(6) NOT NULL
+  ,Gender                    VARCHAR(6) NOT NULL
+  ,Product_line            VARCHAR(22) NOT NULL
+  ,Unit_price              NUMERIC(5,2) NOT NULL
+  ,Quantity                  INTEGER  NOT NULL
+  ,Tax                   NUMERIC(7,4) NOT NULL
+  ,Total                     NUMERIC(8,4) NOT NULL
+  ,Date                      DATE  NOT NULL
+  ,Time                      VARCHAR(8) NOT NULL
+  ,Payment                   VARCHAR(11) NOT NULL
+  ,cogs                      NUMERIC(6,2) NOT NULL
+  ,gross_margin_percentage NUMERIC(11,9) NOT NULL
+  ,gross_income            NUMERIC(7,4) NOT NULL
+  ,Rating                    NUMERIC(3,1) NOT NULL
+);
+
+/* The below query wasn't working because VS CODE isn't given the permissions
+it wasn't even working in MySQL workbench due to [ERROR 3948 (42000), ERROR 2068 (HY000)] and and on the hunt for the reasons 
+I was able to resolve it with the help of StackOverflow(https://stackoverflow.com/questions/59993844/error-loading-local-data-is-disabled-this-must-be-enabled-on-both-the-client,
+https://stackoverflow.com/questions/63361962/error-2068-hy000-load-data-local-infile-file-request-rejected-due-to-restrict)
+
+LOAD DATA LOCAL INFILE "C:/Users/Mohd Rizwan/Downloads/WalmartSalesData.csv"
+INTO TABLE walmart_data
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n';
+
+These restrictions can be removed from MySQL Workbench 8.0 in the following ways.
+First run SET GLOBAL local_infile=1; query and Edit the connection, on the Connection tab, 
+go to the 'Advanced' sub-tab, and in the 'Others:' box add the line 'OPT_LOCAL_INFILE=1'.
+
+*/
+
 SELECT *
-FROM walmart_data
-WHERE (Branch = "A") & (Quantity < 5) & (Unit_price > 60;
+FROM walmart_data;
+
 
 SELECT * 
 FROM walmart_data
 LIMIT 10;
 
-SELECT Branch
+SELECT *
 FROM walmart_data
-LIMIT 3;
+WHERE (Branch = "A") & (Quantity < 5) & (Unit_price > 60);
+
+

baa3f897767a2ce27a98ec5584747eb799e0b3d3
Mohd Rizwan <141806496+Rizwans-github@users.noreply.github.com>
2024-01-10 18:26:27 +0530

    New column

--- walmart.sql
+++ walmart.sql
@@ -30,7 +30,7 @@ it wasn't even working in MySQL workbench due to [ERROR 3948 (42000), ERROR 2068
 I was able to resolve it with the help of StackOverflow(https://stackoverflow.com/questions/59993844/error-loading-local-data-is-disabled-this-must-be-enabled-on-both-the-client,
 https://stackoverflow.com/questions/63361962/error-2068-hy000-load-data-local-infile-file-request-rejected-due-to-restrict)
 
-LOAD DATA LOCAL INFILE "C:/Users/Mohd Rizwan/Downloads/WalmartSalesData.csv"
+LOAD DATA LOCAL INFILE "C:/Users/Mohd Rizwan/Downloads/Walmartwalmart_dataData.csv"
 INTO TABLE walmart_data
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n';
@@ -44,7 +44,8 @@ go to the 'Advanced' sub-tab, and in the 'Others:' box add the line 'OPT_LOCAL_I
 --Ran the first test query to look at all my data
 
 SELECT *
-FROM walmart_data;
+FROM walmart_data
+LIMIT 5;
 
 
 SELECT * 
@@ -56,4 +57,23 @@ FROM walmart_data
 WHERE (Branch = "A") & (Quantity < 5) & (Unit_price > 60)
 LIMIT 5;
 
+------------------------------------------------------------------------------------------------------
+----------------------------- Feature Engineering ----------------------------------------------------
+-- time_of_day
 
+
+ALTER TABLE walmart_data 
+ADD COLUMN time_of_day VARCHAR(20);
+
+UPDATE walmart_data
+SET time_of_day = (
+  CASE
+        WHEN Time BETWEEN "00:00:00" AND "12:00:00" THEN "Morning"
+        WHEN Time BETWEEN "12:01:00" AND "16:00:00" THEN "Afternoon"
+        ELSE "Evening"
+  END
+);
+
+SELECT *
+FROM walmart_data
+LIMIT 5;

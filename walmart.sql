242b8d2c342e944098d70c810e1a276857a915bf
Mohd Rizwan <141806496+Rizwans-github@users.noreply.github.com>
2024-01-18 18:25:08 +0530

    updated

--- walmart.sql
+++ walmart.sql
@@ -136,6 +136,8 @@ FROM walmart_data
 GROUP BY Customer_type
 ORDER BY Total_Rev DESC;
 
+
+
 /*
 Today I got so many errors my sql not showing up in the CLI(command prompt)
 so had to go into enviroment variables of my pc to set it up coding is fun :)

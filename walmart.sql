ef2365c795eee0d09957355412fe9e3bb43d1d30
Mohd Rizwan <141806496+Rizwans-github@users.noreply.github.com>
2024-01-19 07:56:07 +0530

    Update walmart.sql

--- walmart.sql
+++ walmart.sql
@@ -58,82 +58,120 @@ ADD COLUMN month_name VARCHAR(15);
 UPDATE walmart_data
 SET month_name = MONTHNAME(date);
 
-SELECT DISTINCT City
+SELECT 
+  DISTINCT City
 FROM walmart_data;
 
-SELECT DISTINCT City, Branch
+SELECT
+  DISTINCT City,
+  Branch
 FROM walmart_data;
 
-SELECT COUNT(DISTINCT Product_line)
+SELECT 
+  COUNT(DISTINCT Product_line)
 FROM walmart_data;
 
-SELECT payment, COUNT (Payment) AS Most_used
+SELECT 
+  payment, 
+  COUNT (Payment) AS Most_used
 FROM walmart_data
 GROUP BY payment
 ORDER BY Most_used DESC;
 
-SELECT Product_line, COUNT(Product_line) AS CNT
+SELECT 
+  Product_line, 
+  COUNT(Product_line) AS CNT
 FROM walmart_data
 GROUP BY Product_line
 ORDER BY CNT DESC;  
 
-SELECT month_name, SUM(Total) AS Revenue
+SELECT
+  month_name,
+  SUM(Total) AS Revenue
 FROM walmart_data
 GROUP BY month_name
 ORDER BY Revenue DESC;
 
-SELECT month_name, SUM(cogs) AS COG
+SELECT
+  month_name,
+  SUM(cogs) AS COG
 FROM walmart_data
 GROUP BY month_name
 ORDER BY COG DESC;
 
-SELECT Product_line, ROUND(SUM(Total), 2) AS Revenue
+SELECT 
+  Product_line,
+  ROUND(SUM(Total), 2) AS Revenue
 FROM walmart_data
 GROUP BY Product_line
 ORDER BY Revenue DESC;
 
 
-SELECT City, ROUND(SUM(Total), 2) AS Revenue
+SELECT
+  City,
+  ROUND(SUM(Total), 2) AS Revenue
 FROM walmart_data
 GROUP BY City
 ORDER BY Revenue DESC;
 
-SELECT Product_line, ROUND(AVG(Tax), 2) AS VAT
+SELECT
+  Product_line,
+  ROUND(AVG(Tax), 2) AS VAT
 FROM walmart_data
 GROUP BY Product_line
 ORDER BY VAT DESC;
 
-SELECT Branch, SUM(Quantity) As QTY
+SELECT 
+  Branch,
+  SUM(Quantity) As QTY
 FROM walmart_data
 GROUP BY Branch
 HAVING SUM(Quantity) > (SELECT AVG(Quantity) FROM walmart_data);
 
-SELECT Gender, Product_line, COUNT(Gender) AS CNT
+SELECT
+  Gender,
+  Product_line,
+  COUNT(Gender) AS CNT
 FROM walmart_data
 GROUP BY Product_line, Gender
 ORDER BY CNT DESC;
 
-SELECT Product_line, ROUND(AVG(Rating),  2)
+SELECT
+  Product_line,
+  ROUND(AVG(Rating),  2)
 FROM walmart_data
 GROUP BY Product_line
 ORDER BY AVG(Rating) DESC;
 
-SELECT Product_line, ROUND(SUM(Total), 2) AS Total_Revenue, ROUND(AVG(gross_margin_percentage), 2) AS Avg_Gross_Margin_Percentage
+SELECT 
+  Product_line,
+  ROUND(SUM(Total), 2) AS Total_Revenue,
+  ROUND(AVG(gross_margin_percentage), 2) AS Avg_Gross_Margin_Percentage
 FROM walmart_data
 GROUP BY Product_line
 ORDER BY Total_Revenue DESC;
 
-SELECT Branch, Customer_type, COUNT(*) AS Transaction_Count, MIN(Quantity) AS Min_Quantity, MAX(Quantity) AS Max_Quantity, ROUND(AVG(Quantity), 2) AS Avg_Quantity
+SELECT 
+  Branch, 
+  Customer_type, 
+  COUNT(*) AS Transaction_Count, 
+  MIN(Quantity) AS Min_Quantity, 
+  MAX(Quantity) AS Max_Quantity, 
+  ROUND(AVG(Quantity), 2) AS Avg_Quantity
 FROM walmart_data
 GROUP BY Branch, Customer_type
 ORDER BY Transaction_Count DESC;
 
-SELECT time_of_day, COUNT(*) AS Total_Sales
+SELECT 
+  time_of_day,
+  COUNT(*) AS Total_Sales
 FROM walmart_data
 GROUP BY time_of_day
 ORDER BY Total_Sales Desc;
 
-SELECT Customer_type, ROUND(SUM(Total), 2) AS Total_Rev 
+SELECT 
+  Customer_type,
+  ROUND(SUM(Total), 2) AS Total_Rev 
 FROM walmart_data
 GROUP BY Customer_type
 ORDER BY Total_Rev DESC;
@@ -146,34 +184,46 @@ so had to go into enviroment variables of my pc to set it up coding is fun :)
 PS: am not crying
 */
 
-SELECT City, ROUND(AVG(Tax), 2) AS Avg_Tax
+SELECT 
+  City, 
+  ROUND(AVG(Tax), 2) AS Avg_Tax
 FROM walmart_data
 GROUP BY City
 ORDER BY Avg_Tax DESC;
 
 
-SELECT Customer_type, ROUND(AVG(Tax), 2) AS Avg_Tax
+SELECT 
+  Customer_type, 
+  ROUND(AVG(Tax), 2) AS Avg_Tax
 FROM walmart_data
 GROUP BY Customer_type
 ORDER BY Avg_Tax DESC;
 
-SELECT DISTINCT Customer_type
+SELECT 
+  DISTINCT Customer_type
 FROM walmart_data;
 
-SELECT DISTINCT Payment
+SELECT 
+  DISTINCT Payment
 FROM walmart_data;
 
-SELECT Customer_type,COUNT(*) AS CUST
+SELECT 
+  Customer_type,
+  COUNT(*) AS CUST
 FROM walmart_data
 GROUP BY Customer_type
 ORDER BY CUST DESC;
 
-SELECT Gender,COUNT(*) AS CUST
+SELECT 
+  Gender,
+  COUNT(*) AS CUST
 FROM walmart_data
 GROUP BY Gender
 ORDER BY CUST DESC;
 
-SELECT Branch, Gender, COUNT(*) AS CNT
+SELECT 
+  Branch, Gender, 
+  COUNT(*) AS CNT
 FROM walmart_data
 GROUP BY Branch, Gender
 ORDER BY Branch, CNT DESC;
